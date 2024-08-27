d6e6fec376b7f2188fc063b8bf0615f8e38c60f4
Karim-Abdo <145095641+Karim-Abdo@users.noreply.github.com>
2024-08-27 19:54:28 +0000

    changes to chaper 3

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,7 +5,22 @@
 #include <iostream>
 #include <string>
 
+enum class cow_purpose{dairy, meat, hide, pet};
+
+struct cow{
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
 int main(){
+    cow my_cow;
+    my_cow.name = "Betsy";
+    my_cow.age = 3;
+    my_cow.purpose = cow_purpose::dairy;
+    std::cout << "My cow's name is " << my_cow.name << std::endl;
+    std::cout << my_cow.name << " is " << my_cow.age << " years old. " << std::endl;
+    std::cout << my_cow.name << " purpose type is " << (int) my_cow.purpose << " cow." << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
