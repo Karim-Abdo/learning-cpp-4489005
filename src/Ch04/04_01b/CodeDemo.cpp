44618535b1360cde62b5f47b5c671ac9f2879555
Karim-Abdo <145095641+Karim-Abdo@users.noreply.github.com>
2024-08-30 08:10:49 +0000

    chapter 4

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,36 @@
 #include <iostream>
 
 int main(){
+    int a = 1023;
+    bool flag = false;
+    char lttr = 'd';
+
+    if (a > 1000){ 
+        std::cout << "Warning: a is greater than 1000. " << std::endl;
+    }
+
+    if ((a % 2) == 0){
+        std::cout << "a is odd. " << std::endl;
+
+    } else {
+        std::cout << "a is even. " << std::endl;
+    }
+
+    std::cout << "The letter " << lttr << " is ";    
+    if (lttr != 'a' && lttr != 'e' && lttr != 'i' && lttr != 'o' && lttr != 'u' && lttr != 'A' && lttr != 'E' && lttr != 'I' && lttr != 'O' && lttr != 'U'){
+        std::cout << "not ";
+        std::cout << "a vowel. " << std::endl;
+    }
+
+    if (flag){
+        std::cout << "flag is true. " << std::endl;
+    } else {
+        std::cout << "flag is false. " << std::endl;
+    }
     
+     
+    
+
     std::cout << std::endl << std::endl;
     return (0);
 }
